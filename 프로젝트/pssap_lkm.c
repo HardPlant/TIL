68c88345f96a756ab7c9474e93b6410fee2ba77b
HardPlant <abc7988se@naver.com>
2017-10-13 18:18:40 +0900

    Basic Header

---
+++ 프로젝트/pssap_lkm.c
@@ -0,0 +1,12 @@
+#include <linux/init.h> // For init, exit macro
+#include <linux/module.h> // Core header for LKM(Linux Kernel Module)
+#include <linux/kernel.h> // Core header for Kernel
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SecuritySchool");
+MODULE_DESCRIPTION("Airplance Prototype - LKM");
+MODULE_VERSION("0.1");
+
+//Module Init function
+
+
