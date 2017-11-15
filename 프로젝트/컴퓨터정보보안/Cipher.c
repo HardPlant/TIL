17321e57f1eb24855a3a160f2aae4192b4528609
HardPlant <abc7988se@naver.com>
2017-11-15 23:32:55 +0900

    update

---
+++ 프로젝트/컴퓨터정보보안/Cipher.c
@@ -0,0 +1,18 @@
+
+typedef struct rsa_key {
+char block [128];
+} RSA_KEY;
+
+char* AES(char* block, char* key){
+
+}
+
+char* DES(char* block, char* key){
+
+}
+
+char* RSA(int plain, RSA_KEY key){
+int e,d;
+int N = (e-1)*(d-1);
+int cipher = e^key % N;
+}
