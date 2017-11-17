de3f7319a16bc9c79537f4628bc654dbec0085f4
HardPlant <abc7988se@naver.com>
2017-11-17 18:56:02 +0900

    it compiles

--- 프로젝트/컴퓨터정보보안/keyserver.c
+++ 프로젝트/컴퓨터정보보안/keyserver.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #define MAX_PEER 2
+
+typedef int PUBLIC_KEY_TYPE;
+
+typedef struct {
+    PUBLIC_KEY_TYPE pub;
+    PUBLIC_KEY_TYPE pri;
+} KEY_PAIR;
+
 typedef struct{
-    char relations[MAX_PEER][2][256];
     int passwords[MAX_PEER];
     int publics[MAX_PEER];
-    int id_state[MAX_PEER];
+    KEY_PAIR* key_pair[MAX_PEER];
+
     int public_key;
 } KEY_SERVER;
+
+
 
 int id_compare(const void *a, const void *b){
     return ( *(int*)a - *(int*)b );
@@ -30,18 +40,17 @@ int keyserver_get_pw(KEY_SERVER* server, int id){
 
 int keyserver_try_login(KEY_SERVER* server, int id, int pw){
     if(pw != keyserver_get_pw(server,id)) return 0;
-    
     return 1;    
 }
-int keyserver_is_logged_in(KEY_SERVER* server, int id){
-    return 1;
-}
+
 int keyserver_set_public_key(KEY_SERVER* server, int id, int key){
-    server->publics[id] = key;
+    if(server->key_pair[id] == NULL)
+        server->key_pair[id] = (KEY_PAIR*) malloc(sizeof(KEY_PAIR));
+
+    server->key_pair[id]->pub = key;
     return 1;
 }
 
 int keyserver_get_public_key(KEY_SERVER* server, int id){
-    if(!keyserver_is_logged_in(server, id)) return -1;
-    return server->publics[id];
+    return server->key_pair[id]->pub;
 }
