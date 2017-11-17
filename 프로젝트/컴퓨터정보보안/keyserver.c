1abb0bae9be53634516944bda536ee05bd667043
HardPlant <abc7988se@naver.com>
2017-11-17 18:03:04 +0900

    storedId green

--- 프로젝트/컴퓨터정보보안/keyserver.c
+++ 프로젝트/컴퓨터정보보안/keyserver.c
@@ -11,16 +11,17 @@ int id_compare(const void *a, const void *b){
 KEY_SERVER* keyserver_init(){
     return (KEY_SERVER*) malloc(sizeof(KEY_SERVER));
 }
-/*
+
 int keyserver_KEY_set_id(KEY_SERVER* server, int id, int pw){
-//    g_id_pair[id] = pw;
+    server->id_pair[id] = pw;
     return 1;
 }
 
-int keyserver_get_id(int index){
-  //  return g_id_pair[index];
-}
+int keyserver_get_id(KEY_SERVER* server, int index){
 
+    return server->id_pair[index];
+}
+/*
 int keyserver_try_login(int id){
     if(id == key_server_get_id(id)) return 1;
     return 0;
--- 프로젝트/컴퓨터정보보안/test_keyserver.cpp
+++ 프로젝트/컴퓨터정보보안/test_keyserver.cpp
@@ -6,14 +6,15 @@ TEST(KeyServerTest, Init) {
     ASSERT_TRUE(server);
     free(server);
 }
-/*
+
 TEST(KeyServerTest, GetStoredId) { 
     KEY_SERVER* server = keyserver_init();
+    server->id_pair[0]=2;
 
+    ASSERT_EQ(2, keyserver_get_id(server, 0));
     free(server);
-    ASSERT_EQ(2, keyserver_get_id(0));
 }
-
+/*
 TEST(KeyServerTest, TryLoginWithBadId) {
     keyserver_set_id(0,2);
     keyserver_set_id(1,4);
