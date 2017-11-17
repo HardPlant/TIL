9a6db6a3738c33fba2a8661af0a4a047d284375e
HardPlant <abc7988se@naver.com>
2017-11-17 15:52:02 +0900

    update

--- 프로젝트/컴퓨터정보보안/Cipher.c
+++ 프로젝트/컴퓨터정보보안/Cipher.c
@@ -13,10 +13,46 @@ char* DES(char* block, char* key){
 
 }
 
-char* RSA(int plain, RSA_KEY key){
-    RSA_TYPE p, q, N, pi;
+typedef struct {
+    RSA_TYPE N;
+    RSA_TYPE e; // public key
+    RSA_TYPE d; // private key
+} RSA_KEY_PAIR;
 
+typedef struct {
+    RSA_KEY_PAIR (*init)();
+    RSA_TYPE (*encrypt)(RSA_TYPE msg, RSA_KEY_PAIR pair);
+    RSA_TYPE (*decrypt)(RSA_TYPE enc, RSA_KEY_PAIR pair);
+
+    RSA_KEY_PAIR pair;
+} *RSA_CONTROLLER;
+
+RSA_KEY_PAIR RSA_init(int plain, RSA_KEY key){
+    RSA_TYPE p, q, N, pi, e, d;
+    RSA_KEY_PAIR* pair = malloc(sizeof(RSA_KEY_PAIR));
     rsa_get_coprime(p,q);
 
+    N = rsa_multi(p,q);
+    pi = rsa_multi(rsa_minus(p,1),rsa_minus(q,1));
+
+    e = rsa_get_minor_prime(pi);
+
+    d = rsa_gcd(e, pi);
+
+    rsa_set_public(pair, N, e);
+    rsa_set_private(pair, d);
+
+    return pair;
+}
+
+RSA_TYPE rsa_encrypt(RSA_TYPE msg, RSA_KEY_PAIR pair){
+    return rsa_mod(rsa_exp(msg, pair->e),pair->N);
+}
+
+RSA_TYPE rsa_decrypt(RSA_TYPE enc, RSA_KEY_PAIR pair){
+    return rsa_mod(rsa_exp(enc, pair->d),pair->N);
+}
+
+RSA_TYPE rsa_get_minor_prime(RSA_TYPE pi){
     
 }
