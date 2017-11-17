6a1e8d045bbbdf4b8cac504822b28245974d066f
HardPlant <abc7988se@naver.com>
2017-11-17 17:33:36 +0900

    test all passed

--- 프로젝트/컴퓨터정보보안/test_keyserver.cpp
+++ 프로젝트/컴퓨터정보보안/test_keyserver.cpp
@@ -1,2 +1,6 @@
 #include <gtest/gtest.h>
 #include "keyserver.c"
+
+TEST(KeyServerTest, Init) { 
+    ASSERT_EQ(1, keyserver_init());
+}
--- 프로젝트/컴퓨터정보보안/test_server.cpp
+++ 프로젝트/컴퓨터정보보안/test_server.cpp
@@ -1,2 +1,6 @@
 #include <gtest/gtest.h>
 #include "server.c"
+
+TEST(ServerTest, Init) { 
+    ASSERT_EQ(1, server_init());
+}
