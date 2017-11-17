88747e0ef655c1d8953998e838db96a4f5e7e7a5
HardPlant <abc7988se@naver.com>
2017-11-17 21:47:14 +0900

    main socket

--- 프로젝트/컴퓨터정보보안/main.c
+++ 프로젝트/컴퓨터정보보안/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 #include <pthread.h>
 /*
 #include "client.c"
@@ -7,12 +11,13 @@
 */
 void startKeyServer();
 void startChat();
-void* keyserver_init(void* data){ printf("Keyserver\n");}
-void* client_init(void* data){ printf("Client\n");}
-void* server_init(void* data){ printf("Server\n");}
+void* keyserver_start(void* data);
+void* client_start(void* data);
+void* server_start(void* data);
 
 int main(int argc, char* argv[]){
     char input = '0';
+
     printf("키 서버입니까?: (Y/N)");
     while(!(input == 'Y' || input == 'N'))
         input = getchar();
@@ -25,16 +30,99 @@ int main(int argc, char* argv[]){
 
 void startKeyServer(){
     pthread_t key_server;
-    pthread_create(&key_server, NULL, keyserver_init, NULL);
+    int server_port;
+
+    pthread_create(&key_server, NULL, keyserver_start, (void*)&server_port);
     pthread_join(key_server, NULL);
 }
 void startChat(){
+    void* client_args[2];
+    char client_addr[100];
+    int client_port;
+    int server_port;
+
+    client_args[0] = (void*)client_addr;
+    client_args[1] = (void*)&client_port;
+
+
     pthread_t client;
     pthread_t server;
-    pthread_create(&client, NULL, client_init, NULL);
-    pthread_create(&server, NULL, server_init, NULL);
+    pthread_create(&client, NULL, client_start, (void*)client_args);
+    pthread_create(&server, NULL, server_start, (void*)server_port);
     pthread_join(client, NULL);
     pthread_join(server, NULL);
 }
 
 
+void* keyserver_start(void* data){
+    int port = *( (int*)data );
+    int listen_fd, connection_fd;
+    struct sockaddr_in serve_addr;
+    pthread_t handler;
+    printf("Keyserver\n");
+    
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&serve_addr,sizeof(serve_addr));
+
+    serve_addr.sin_family = AF_INET;
+    serve_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    serve_addr.sin_port = htons(port);
+
+    bind(listen_fd, (struct sockaddr*) &serve_addr, sizeof(serve_addr));
+
+    listen(listen_fd, 10);
+    
+    while(1){
+        connetion_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
+        pthread_create(handler, NULL, keyserver_handler, &connetion_fd);
+    }   
+}
+void* keyserver_handler(void* data){
+    int conn_fd = *( (int*)data );
+}
+void* client_start(void* data){
+    char* dest_addr = (char*)data[0];
+    int port = *( (int*)data[1] );
+    printf("Client\n");
+
+    int sockfd;
+    struct sockaddr_in client_addr;
+    pthread_t handler;
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&client_addr,sizeof(client_addr));
+
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(port);
+    inet_pton(AF_INET, dest_addr, &(client_addr.sin_addr));
+    connect(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+    while(1){
+
+    }
+}
+
+void* server_start(void* data){
+    int port = *( (int*)data );
+    int listen_fd, connection_fd;
+    struct sockaddr_in serve_addr;
+    pthread_t handler;
+    printf("Server\n");
+    
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&serve_addr,sizeof(serve_addr));
+
+    serve_addr.sin_family = AF_INET;
+    serve_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    serve_addr.sin_port = htons(port);
+
+    bind(listen_fd, (struct sockaddr*) &serve_addr, sizeof(serve_addr));
+
+    listen(listen_fd, 10);
+    
+    while(1){
+        connetion_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
+        pthread_create(handler, NULL, server_handler, &connetion_fd);
+    }   
+}
+void* server_handler(void* data){
+    int conn_fd = *( (int*)data );
+}
