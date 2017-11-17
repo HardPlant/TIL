f4ecd80c77129060b8fe048aa2816b35c758eb53
HardPlant <abc7988se@naver.com>
2017-11-17 17:42:15 +0900

    update

--- 프로젝트/컴퓨터정보보안/keyserver.c
+++ 프로젝트/컴퓨터정보보안/keyserver.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 
-static int id[100];
+static int g_id[100];
 
 int id_compare(const void *a, const void *b){
     return ( *(int*)a - *(int*)b );
 }
-
 
 int keyserver_init(){
     return 1;
 }
 
 int keyserver_get_id(int index){
-    return id[index];
+    int* id = (int*) bsearch(&index, g_id
+        , sizeof(g_id)/sizeof(int), sizeof(int), id_compare);
+    return *id;
 }
 
 int keyserver_try_login(int id){
--- 프로젝트/컴퓨터정보보안/test_keyserver.cpp
+++ 프로젝트/컴퓨터정보보안/test_keyserver.cpp
@@ -6,5 +6,5 @@ TEST(KeyServerTest, Init) {
 }
 
 TEST(KeyServerTest, GetStoredId) { 
-    ASSERT_EQ(1, keyserver_get_id(0));
+    ASSERT_EQ(0, keyserver_get_id(0));
 }
