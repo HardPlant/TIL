31506e3e240d17118666970461257e5735dbf90c
HardPlant <abc7988se@naver.com>
2017-11-17 18:12:50 +0900

    trylogin fails

--- 프로젝트/컴퓨터정보보안/keyserver.c
+++ 프로젝트/컴퓨터정보보안/keyserver.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
-
+#define MAX_PEER 2
 typedef struct{
-    int id_pair[2];
+    int id_pair[MAX_PEER];
+    int id_state[MAX_PEER];
+    int public_key;
 } KEY_SERVER;
 
 int id_compare(const void *a, const void *b){
     return ( *(int*)a - *(int*)b );
 }
 
-KEY_SERVER* keyserver_init(){
-    return (KEY_SERVER*) malloc(sizeof(KEY_SERVER));
+KEY_SERVER* keyserver_init(int public_key){
+    KEY_SERVER* server = (KEY_SERVER*) malloc(sizeof(KEY_SERVER));
+    server->public_key = public_key;
+    return server;
 }
 
 int keyserver_set_id(KEY_SERVER* server, int id, int pw){
@@ -23,6 +27,6 @@ int keyserver_get_id(KEY_SERVER* server, int index){
 }
 
 int keyserver_try_login(KEY_SERVER* server, int id){
-    if(id == key_server_get_id(server,id)) return 1;
+    if(id == keyserver_get_id(server,id)) return 1;
     return 0;
 }
--- 프로젝트/컴퓨터정보보안/test_keyserver.cpp
+++ 프로젝트/컴퓨터정보보안/test_keyserver.cpp
@@ -2,13 +2,15 @@
 #include "keyserver.c"
 
 TEST(KeyServerTest, Init) { 
-    KEY_SERVER* server = keyserver_init();
+    int public_key = 0;
+    KEY_SERVER* server = keyserver_init(public_key);
     ASSERT_TRUE(server);
+    ASSERT_EQ(server->public_key, public_key);
     free(server);
 }
 
 TEST(KeyServerTest, GetStoredId) { 
-    KEY_SERVER* server = keyserver_init();
+    KEY_SERVER* server = keyserver_init(0);
     keyserver_set_id(server,0,2);
 
     ASSERT_EQ(2, keyserver_get_id(server, 0));
@@ -16,20 +18,20 @@ TEST(KeyServerTest, GetStoredId) {
 }
 
 TEST(KeyServerTest, TryLoginWithBadId) {
-    KEY_SERVER* server = keyserver_init();
+    KEY_SERVER* server = keyserver_init(0);
 
     keyserver_set_id(server, 0,2);
     keyserver_set_id(server, 1,4);
-    ASSERT_EQ(1, keyserver_try_login(0));
+    ASSERT_EQ(1, keyserver_try_login(server,0));
     
     free(server);
 }
 TEST(KeyServerTest, TryLoginWithGoodId) {
-    KEY_SERVER* server = keyserver_init();
+    KEY_SERVER* server = keyserver_init(0);
 
     keyserver_set_id(server, 0,2);
     keyserver_set_id(server, 1,4);
-    ASSERT_EQ(2, keyserver_try_login(0));
+    ASSERT_EQ(2, keyserver_try_login(server,0));
     
     free(server);
 }
